3a13f5b92e0fcdbdc57108688dc189c6d7756641
nuratann <kadyrov.atan@gmail.com>
2024-06-30 17:57:54 +0600

    freelancer-service init commit

---
+++ freelancer-service/src/main/java/kg/kyzmatapp/freelancerservice/FreelancerServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.kyzmatapp.freelancerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerServiceApplication.class, args);
+	}
+
+}
---
+++ freelancer-service/src/test/java/kg/kyzmatapp/freelancerservice/FreelancerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.kyzmatapp.freelancerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
