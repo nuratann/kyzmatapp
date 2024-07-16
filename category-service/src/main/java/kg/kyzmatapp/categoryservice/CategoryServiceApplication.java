4c13783ebf4a77c35b9afa7524e03bb1f29b2e80
nuratann <kadyrov.atan@gmail.com>
2024-07-16 15:16:34 +0600

    init commit for category-service

---
+++ category-service/src/main/java/kg/kyzmatapp/categoryservice/CategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package kg.kyzmatapp.categoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryServiceApplication.class, args);
+    }
+
+}
---
+++ category-service/src/test/java/kg/kyzmatapp/categoryservice/CategoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.kyzmatapp.categoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
